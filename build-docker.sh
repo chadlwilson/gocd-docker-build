7ad87b0e7d8ea964afaec20e0b5a09155b1457d6
Chad Wilson <chadw@thoughtworks.com>
2021-07-08 21:22:24 +0800

    Change to building Docker image based on a verified, pre-built server image; rather than rebuilding it from scratch

---
+++ build-docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+gocd_target="${GOCD_TARGET_VERSION:-21.2.0}"
+
+function download_server_zip() {
+  curl -fsSLO https://download.gocd.org/releases.json
+  jq ".[] | select(.go_version == \"${gocd_target}\")" < releases.json > target.json
+  gocd_target_download_url="https://download.gocd.org/binaries/$(jq -r '.go_full_version' < target.json)/$(jq -r '.generic.server.file' < target.json)"
+
+  curl -fsSLO "${gocd_target_download_url}"
+  gocd_target_server_zip=$(greadlink -f "$(find . -name '*.zip' -print)")
+  echo "$(jq -r '.generic.server.sha256sum' < target.json) ${gocd_target_server_zip}" | sha256sum -c
+}
+
+function docker_gradle() {
+  ./gradlew docker:gocd-server:assemble -PdockerbuildServerZipLocation="${gocd_target_server_zip}"
+}
+
+download_server_zip
+docker_gradle
